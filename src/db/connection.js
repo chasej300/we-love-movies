10021d89460c512e064deca998306766e572d6c8
Chase Johnson <55564950+chasej300@users.noreply.github.com>
2022-08-25 16:47:43 -0600

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
-const config = require("../../mhh[3~hhhhhhhhhhhh[3~[3~[3~hhhh/knexfile")[environment];
+const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
 module.exports = knex;
